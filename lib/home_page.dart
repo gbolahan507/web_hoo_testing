7b2b68554ec12de6d4d5ebf94852898ca2b52f70
Habeeb <ahabeeblahi1@gmail.com>
2023-09-21 03:16:57 +0100

    color change to orange

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,7 +7,7 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.pink
+        color: Colors.orange
       ),
     );
   }
