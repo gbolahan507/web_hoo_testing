152a05c5042de703718ac6d48580a12e3f96fca4
Habeeb <ahabeeblahi1@gmail.com>
2023-10-22 10:16:38 +0100

    colors changed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,7 +7,7 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.white
+        color: Colors.deepOrange
       ),
     );
   }
